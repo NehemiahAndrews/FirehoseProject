df54b7777acdd37e118ff7989d8ad4b6e30c4c13
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-06-30 08:03:49 +0000

    Added Comments to Schrodinger

--- schrodinger.rb
+++ schrodinger.rb
@@ -7,8 +7,10 @@ puts "# Coded by Nehemiah Andrews #"
 puts "### I am a coding machine ###"
 puts "#############################"
 
+# Create Array
 pets = []
 
+# Populate array with data
 pets << "Cat"
 pets << "Dog"
 pets << "Calico Cat"
@@ -21,14 +23,17 @@ pets << "Dog day summer"
 pets << "Kitty Cat"
 pets << "Kit Kat"
 
+# Enumerate through array items, posting original values, removing the word "Cat", and showing the change
 pets.each do |f|
   puts "Behold, what began as a #{f}"
   f.gsub! 'Cat', ''
   puts "is now a #{f}"  
 end
 
+# Exiting looped to demonstrate that local variable has been destroyed
 puts "\nAnd to prove that they have truly changed, and this isn't just an illusion:\n"
 
-pets.each do |f|
-  puts "#{f}"  
+# Looping through array contents again with new local variable to demonstrate that change is global
+pets.each do |p|
+  puts "#{p}"  
 end
